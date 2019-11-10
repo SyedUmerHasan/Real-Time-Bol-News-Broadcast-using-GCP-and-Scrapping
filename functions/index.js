10db77eb6757d5fa40a7b5835335582c5e12d8e1
syedumerhasan <umerhasan2010@gmail.com>
2019-11-10 20:15:13 +0500

    all arary working properly

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,40 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const app = express();
 
+const fetch = require('node-fetch');
+const convert = require('xml-js');
+
+const cors = require('cors')({ origin: true });
+app.use(cors);
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+    response.send("Hello from Firebase!");
 });
+
+app.get('/', (req, res) => {
+    res.send("I am umer");
+});
+
+app.get('/api', (req, res) => {
+    let dataAsJson = {};
+
+    fetch('https://www.bolnews.com/feed/')
+    .then(response => response.text())
+    .then(str => {
+        dataAsJson = JSON.parse(convert.xml2json(str));
+    })
+    .then(() => {
+        return res.send(dataAsJson);
+    })
+    .catch((err) => {
+        console.log(err);
+        return res.send({"status" : "fail", "error"  : err});
+
+    })
+
+});
+
+exports.app = functions.https.onRequest(app);
