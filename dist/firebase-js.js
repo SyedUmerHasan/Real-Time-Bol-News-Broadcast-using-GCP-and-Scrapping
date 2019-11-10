e06e1ecf58fae686c70c8f714b3197babba970f0
syedumerhasan <umerhasan2010@gmail.com>
2019-11-10 10:54:54 +0500

    Adding cloud functions

---
+++ dist/firebase-js.js
@@ -0,0 +1,11 @@
+<script>
+
+    <script src="https://www.gstatic.com/firebasejs/7.2.3/firebase-app.js"></script>
+
+    <script src="https://www.gstatic.com/firebasejs/7.2.3/firebase-analytics.js"></script>
+
+    <script src="https://www.gstatic.com/firebasejs/7.2.3/firebase-auth.js"></script>
+    
+    <script src="https://www.gstatic.com/firebasejs/7.2.3/firebase-firestore.js"></script>
+
+</script>
