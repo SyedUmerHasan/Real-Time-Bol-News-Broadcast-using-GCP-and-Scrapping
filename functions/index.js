394a6d9757466ffb23b990e28d35f9690accbeaf
syedumerhasan <umerhasan2010@gmail.com>
2019-11-11 19:37:01 +0500

    Scrapper running successfully

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,17 @@
 const functions = require('firebase-functions');
 const express = require('express');
 const app = express();
+const scrapper = express();
+
+const cheerio_request = require("request");
+const cheerio = require("cheerio");
 
 const fetch = require('node-fetch');
 const convert = require('xml-js');
 
 const cors = require('cors')({ origin: true });
 app.use(cors);
+scrapper.use(cors);
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -38,3 +43,21 @@ app.get('/api', (req, res) => {
 });
 
 exports.app = functions.https.onRequest(app);
+
+scrapper.get('/', (request, response) => {
+    cheerio_request("https://www.bolnews.com/sports/2019/11/peshawar-to-host-33rd-national-games-after-9-years/",(err, res, html)=>{
+        console.log(res.statusCode )
+        if(!err && res.statusCode == 200)
+        {
+            const $ = cheerio.load(html);
+
+            response.send($(".featured-img").children("img").attr("src"))
+
+        }
+        else{
+            response.send("Error : ");
+        }
+    })  
+    
+});
+exports.scrapper = functions.https.onRequest(scrapper);
